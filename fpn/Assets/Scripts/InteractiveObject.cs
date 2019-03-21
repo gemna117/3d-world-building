1612b29b9105cf2a4392435665f9c08588628b51
Megan Alonzo <Megan.Alonzo@iam.local>
2019-03-21 17:43:45 -0500

    scripting

---
+++ fpn/Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour, IInteractive
+{
+    [SerializeField]
+    private string displayText = nameof(InteractiveObject);
+    public string DisplayText => displayText;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void interactwith()
+    {
+        try
+        {
+        audioSource.Play();
+        }
+        catch (System.Exception)
+        {
+            throw new System.Exception("");
+        }
+        Debug.Log($"player just interacted with {gameObject.name}.");
+    }
+}
---
+++ fpn/Assets/Scripts/LookedAtInteractiveDisplayText.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LookedAtInteractiveDisplayText : MonoBehaviour
+{
+    private IInteractive lookedAtInteractive;
+    private Text displayText;
+
+    private void Awake()
+    {
+        displayText = GetComponent<Text>();
+        updatedisplaytext();
+    }
+
+    private void updatedisplaytext()
+    {
+        if (lookedAtInteractive != null)
+            displayText.text = lookedAtInteractive.DisplayText;
+        else
+            displayText.text = string.Empty;
+    }
+
+    private void onlookedatinteractivechanged(IInteractive newLookedAtInteractive)
+    {
+        lookedAtInteractive = newLookedAtInteractive;
+        updatedisplaytext();
+    }
+
+    #region event subscription / unsubscription
+    private void OnEnable()
+    {
+        DetectLookedAtInteractive.LookedAtInteractiveChanged += onlookedatinteractivechanged;
+    }
+
+    private void OnDisable()
+    {
+        DetectLookedAtInteractive.LookedAtInteractiveChanged += onlookedatinteractivechanged;
+    }
+    #endregion
+}
