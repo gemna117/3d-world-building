df966e9635f56bfdeecb7bb3738d7e4e70ee7476
gemna117 <megan.alonzo@loop.colum.edu>
2019-04-04 10:21:28 -0700

    title menu

---
+++ fpn/Assets/Scripts/InteractWithLookedAt.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithLookedAt : MonoBehaviour
+{
+    private IInteractive lookedAtInteractive;
+    void Update()
+    {
+        if (Input.GetButton("interact") && detectLookedAtInteractive.lookedAtInteractive != null)
+        {
+            Debug.Log("player pressed the interact button");
+            lookedAtInteractive.interactivewith();
+            
+        }
+    }
+
+    private void onlookedatinteractivechanged(IInteractive newLookedAtInteractive)
+    {
+        lookedAtInteractive = newLookedAtInteractive;
+    }
+
+    #region event subscription / unsubscription
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+    #endregion
+}
---
+++ fpn/Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    [SerializeField]
+    private string gamescenename;
+
+    public void loadgamescene()
+    {
+        SceneManager.LoadScene(gamescenename);
+    }
+
+    public void exitscene()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
