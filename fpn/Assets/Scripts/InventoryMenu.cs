7a736426ea7ecb0fb6aab3b0a4d5e406013ef7f6
gemna117 <megan.alonzo@loop.colum.edu>
2019-04-25 09:56:10 -0700

    more inventory

---
+++ fpn/Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private static InventoryMenu instance;
+
+    public InventoryMenu instance
+    {
+        get
+        {
+            if (instance == null)
+                throw new System.Exception("");
+            return instance;
+        }
+        private set { instance = value; }
+    }
+
+    private void awake()
+    {
+        if (instance == null)
+        instance = this;
+    }
+}
